5abbdaf0f46cdd3058b311f3186a4a044639fee5
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2024-12-11 17:59:51 +0100

    Mise à jour de l'URL du site et de la base dans la configuration Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yoanbernabeu.github.io',
-  base: 'PrepaVideoCV/',
+  site: 'https://yoanbernabeu.github.io/PrepaVideoCV/',
   integrations: [tailwind()],
 });
